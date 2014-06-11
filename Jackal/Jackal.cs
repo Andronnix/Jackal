3e57b005c3b83ace743d7a167b7613bcec196c26
Andrey Kokorev <a.d.kokorev@yandex.ru>
2014-06-11 19:22:39 +0400

    Hello world commit
    Added lib file, green surface and rectangle

--- Jackal/Jackal.cs
+++ Jackal/Jackal.cs
@@ -1,13 +1,42 @@
 ﻿using System;
 using System.Text;
+using System.Drawing;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
 
 namespace Jackal
 {
-    class Jackal
+    class Jackal : GameWindow
     {
-        public Jackal()
+        public Jackal() : base()
         {
+            this.Run();
+        }
 
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            GL.ClearDepth(1.0);
+            GL.ClearColor(Color.Green);
+            GL.Enable(EnableCap.DepthTest);
+            GL.DepthFunc(DepthFunction.Lequal);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.Begin(PrimitiveType.LineLoop);
+            GL.Color3(Color.Azure);
+            GL.Vertex2(0.7f, 0.7f);
+            GL.Vertex2(0.7f, -0.7f);
+            GL.Vertex2(-0.7f, -0.7f);
+            GL.Vertex2(-0.7f, 0.7f);
+
+            GL.End();
+            SwapBuffers();
         }
     }
 }
